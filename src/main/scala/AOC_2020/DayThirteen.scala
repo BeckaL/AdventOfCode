25032719595096fabcabdcff726dc7f2734b6e1c
Rebecca Lelew <rebecca.lelew@springernature.com>
2020-12-13 15:49:02 +0000

    Add in day 13

--- src/main/scala/AOC_2020/DayThirteen.scala
+++ src/main/scala/AOC_2020/DayThirteen.scala
@@ -2,12 +2,41 @@ package AOC_2020
 
 import shared.DayChallenge
 
-object DayThirteen extends DayChallenge[Int, Int] {
-  override def partOne(l: List[String]): Int = {0}
+object DayThirteen extends DayChallenge[Int, Long] {
+  override def partOne(l: List[String]): Int = {
+    val earliestTimestamp = l.head.toInt
+    val (minBus, minTime) = getBuses(l)
+      .map { b => b -> findClosestTo(earliestTimestamp, b.route).toInt }
+      .minBy(_._2)
+    (minTime - earliestTimestamp) * minBus.route.toInt
+  }
 
-  override def partTwo(l: List[String]): Int = {0}
+  override def partTwo(l: List[String]): Long =
+    getBuses(l).reduce((b1, b2) => getCombinedBus(b1,b2)).timestamp
 
-  override val testData: List[String] = List()
-  override val expectedPartOne: Option[Int] = Some(1000000)
-  override val expectedPartTwo: Option[Int] = Some(100000)
+  private def getBuses(l: List[String]): List[Bus] =
+    l(1).split(",").zipWithIndex.collect { case (busNo, i) if busNo != "x" => Bus(busNo.toInt, i) }.toList
+
+  private def getCombinedBus(bus1: Bus, bus2: Bus): Bus = {
+    val newRoute = bus1.route * bus2.route
+    def go(i: Long, step: Long): Bus = if (bus2.worksWith(i))
+      Bus(newRoute, newRoute - i)
+    else
+      go(i + step, step)
+    go(bus1.route - bus1.remainder, bus1.route)
+  }
+
+  case class Bus(route: Long, remainder: Long) {
+    def worksWith(i: Long): Boolean = (i + remainder) % route == 0
+    def timestamp: Long =  route - remainder
+  }
+
+  private def findClosestTo(i: Long, busNo: Long): Long = ((i.toFloat / busNo).ceil * busNo).toLong
+
+  override val testData: List[String] = List(
+    "939",
+    "7,13,x,x,59,x,31,19"
+  )
+  override val expectedPartOne: Option[Int] = Some(295)
+  override val expectedPartTwo: Option[Long] = Some(1068781)
 }
--- src/test/scala/AOC_2020/DayThirteenTest.scala
+++ src/test/scala/AOC_2020/DayThirteenTest.scala
@@ -3,19 +3,19 @@ package AOC_2020
 import org.scalatest.FlatSpec
 import shared.{DayChallenge, DayChallengeBehaviours}
 
-class DayThirteenTest extends FlatSpec with DayChallengeBehaviours[Int, Int] {
+class DayThirteenTest extends FlatSpec with DayChallengeBehaviours[Int, Long] {
   override val day = "Day thirteen"
   override val file: String = "Day13Input"
-  override val dayChallenge: DayChallenge[Int,Int] = DayThirteen
+  override val dayChallenge: DayChallenge[Int,Long] = DayThirteen
 
   private val testInput = dayChallenge.testData
 
   day should behave like partOneWorkingCorrectly(testInput, dayChallenge.expectedPartOne.get)
 
-  it should behave like partOneWorksOnRealData(1)
+  it should behave like partOneWorksOnRealData(2406)
 
   it should behave like partTwoWorksOnTestData(testInput, dayChallenge.expectedPartTwo.getOrElse(0))
 
-  it should behave like partTwoWorksOnRealData(1)
+  it should behave like partTwoWorksOnRealData("225850756401039".toLong)
 
 }
