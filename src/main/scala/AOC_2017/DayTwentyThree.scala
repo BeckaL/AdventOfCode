a38bbdba79c03eafa9c4eb50a3c52df443810e9e
BeckaL <rebecca.lelew@gmail.com>
2021-07-17 22:02:36 +0100

    Refactor Day 23 2017

--- src/main/scala/AOC_2017/DayTwentyThree.scala
+++ src/main/scala/AOC_2017/DayTwentyThree.scala
@@ -6,25 +6,23 @@ import shared.{DayChallenge, TestData}
 import scala.annotation.tailrec
 
 object DayTwentyThree extends DayChallenge[Int, Int] {
-  override def partOne(l: List[String]): Int = {
-    val registers =
-      Map("a" -> 0, "b" -> 0, "c" -> 0, "d" -> 0, "e" -> 0, "f" -> 0, "g" -> 0, "h" -> 0)
-    findNoOfTimesMulCalled(0, l, registers, 0)
-  }
+  override def partOne(l: List[String]): Int = findNoOfTimesMulCalled(l)
 
-  @tailrec
-  private def findNoOfTimesMulCalled(i: Int, instructions: List[String], registers: Map[String, Int], mulCalled: Int): Int =
-    if (!instructions.indices.contains(i))
-      mulCalled
-    else {
-      val (newI, newRegisters, newMulCalled) = instructions(i).split(" ").toList match {
-        case "set" :: x :: y :: Nil => (i + 1, registers.set(x, y), mulCalled)
-        case "sub" :: x :: y :: Nil => (i + 1, registers.sub(x, y), mulCalled)
-        case "mul" :: x :: y :: Nil => (i + 1, registers.mul(x, y), mulCalled + 1)
-        case "jnz" :: x :: y :: Nil => (i + registers.newOffset(x, y), registers, mulCalled)
-      }
-      findNoOfTimesMulCalled(newI, instructions, newRegisters, newMulCalled)
-    }
+  private def findNoOfTimesMulCalled(instructions: List[String]): Int = {
+    @tailrec
+    def go(i: Int, registers: Map[String, Int], mulCalled: Int): Int =
+      if (!instructions.indices.contains(i))
+        mulCalled
+      else
+        instructions(i).split(" ").toList match {
+          case "set" :: x :: y :: Nil => go(i + 1, registers.set(x, y), mulCalled)
+          case "sub" :: x :: y :: Nil => go(i + 1, registers.sub(x, y), mulCalled)
+          case "mul" :: x :: y :: Nil => go(i + 1, registers.mul(x, y), mulCalled + 1)
+          case "jnz" :: x :: y :: Nil => go(i + registers.newOffset(x, y), registers, mulCalled)
+        }
+
+    go(0, Map("a" -> 0, "b" -> 0, "c" -> 0, "d" -> 0, "e" -> 0, "f" -> 0, "g" -> 0, "h" -> 0), 0)
+  }
 
   override def partTwo(l: List[String]): Int =
     (0 to 1000).foldLeft(0) { case (counter, i) => if (notPrime(108400 + (17 * i))) counter + 1 else counter }
