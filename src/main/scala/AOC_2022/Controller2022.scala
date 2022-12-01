7e9fa0373a83b80360b9952d63efd64640b1178b
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-12-01 17:17:27 +0000

    Add script to create day

--- src/main/scala/AOC_2022/Controller2022.scala
+++ src/main/scala/AOC_2022/Controller2022.scala
@@ -11,7 +11,7 @@ object Controller2022 extends YearController {
   def getDayChallengeAndData(day: String): (DayChallenge[_, _], TestData[_, _]) =
     day match {
       case "1"  => (DayOne, DayOneData)
-//      case "2"  => (DayTwo, DayTwoData)
+      case "2"  => (DayTwo, DayTwoData)
 //      case "3"  => (DayThree, DayThreeData)
 //      case "4"  => (DayFour, DayFourData)
 //      case "5"  => (DayFive, DayFiveData)
