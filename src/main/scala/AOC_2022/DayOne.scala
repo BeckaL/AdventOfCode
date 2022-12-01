72c4a6a382ca613988a217456a727e049b607efc
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-12-01 07:50:31 +0000

    Day 1 2022

--- src/main/scala/AOC_2022/DayOne.scala
+++ src/main/scala/AOC_2022/DayOne.scala
@@ -3,19 +3,31 @@ package AOC_2022
 import shared.{DayChallenge, TestData}
 
 object DayOne extends DayChallenge[Int, Int] {
-  override def partOne(l: List[String]): Int = {
-    1
-  }
+  override def partOne(l: List[String]): Int = parse(l).max
 
-  override def partTwo(l: List[String]): Int = {
-    2
-  }
+  override def partTwo(l: List[String]): Int = parse(l).sorted(Ordering[Int].reverse).take(3).sum
+
+  private def parse(l: List[String]): List[Int] =
+    l.mkString(",").split(",,").toList.map(x => x.split(",").map(_.toInt).sum)
 }
 
 object DayOneData extends TestData[Int, Int] {
   override val testData: List[String] = List(
-    """"""
+    "1000",
+    "2000",
+    "3000",
+    "",
+    "4000",
+    "",
+    "5000",
+    "6000",
+    "",
+    "7000",
+    "8000",
+    "9000",
+    "",
+    "10000",
   )
-  override val expectedPartOne: Option[Int] = Some(0)
-  override val expectedPartTwo: Option[Int] = Some(0)
+  override val expectedPartOne: Option[Int] = Some(24000)
+  override val expectedPartTwo: Option[Int] = Some(45000)
 }
