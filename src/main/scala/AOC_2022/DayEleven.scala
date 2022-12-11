3e31c24c46ac2b5674e8639c23ac9cdb81152bd9
Becka <rebecca.lelew@gmail.com>
2022-12-11 17:31:59 +0000

    Day 11 tidy ups

--- src/main/scala/AOC_2022/DayEleven.scala
+++ src/main/scala/AOC_2022/DayEleven.scala
@@ -2,23 +2,21 @@ package AOC_2022
 
 import shared.{DayChallenge, TestData, Helpers}
 
-object DayEleven extends DayChallenge[BigInt, Long] with Helpers {
-  override def partOne(l: List[String]): BigInt =
-    productOfBusiestMonkeys(monkeys(l), 20, _ / 3)
+object DayEleven extends DayChallenge[Long, Long] with Helpers {
+  override def partOne(l: List[String]): Long =
+    productOfTwoBusiestMonkeys(monkeys(l), 20, _ / 3)
 
   override def partTwo(l: List[String]): Long = {
     val ms = monkeys(l)
-    val lcd = ms.map(_._2.divBy).product
-    productOfBusiestMonkeys(ms, 10000, _ % lcd)
+    val lcm = ms.map(_._2.divisibleBy).product
+    productOfTwoBusiestMonkeys(ms, 10000, _ % lcm)
   }
 
-  private def productOfBusiestMonkeys(monkeys: Map[Int, Monkey], iterations: Int, worryReducer: Long => Long): Long =
+  private def productOfTwoBusiestMonkeys(monkeys: Map[Int, Monkey], iterations: Int, worryReducer: Long => Long): Long =
     (0 until iterations)
       .foldLeft(monkeys) { case (ms, _) => round(ms, worryReducer) }
-      .toList
-      .sortBy { case (_, m) => m.inspected }.takeRight(2)
-      .map(_._2.inspected.toLong)
-      .product
+      .map{ case (_, m) => m.inspected.toLong }
+      .toList.sorted.takeRight(2).product
 
   private def round(initalMs: Map[Int, Monkey], reducer: Long => Long): Map[Int, Monkey] =
     initalMs.keys.toList.sorted.foldLeft(initalMs) { case (ms, i) => turn(ms(i), ms, reducer)}
@@ -26,7 +24,7 @@ object DayEleven extends DayChallenge[BigInt, Long] with Helpers {
   private def turn(monkey: Monkey, monkeys: Map[Int, Monkey], reducer: Long => Long): Map[Int, Monkey] =
     monkey.items.foldLeft(monkeys) { case (ms, item) =>
       val newWorryLevel = reducer(monkey.op(item))
-      val throwTo = if (newWorryLevel % monkey.divBy == 0) ms(monkey.ifTrue) else ms(monkey.ifFalse)
+      val throwTo = if (newWorryLevel % monkey.divisibleBy == 0) ms(monkey.ifTrue) else ms(monkey.ifFalse)
       ms.updated(throwTo.i, throwTo.copy(items = throwTo.items :+ newWorryLevel))
     }.updated(monkey.i, monkey.copy(items = List(), inspected = monkey.inspected + monkey.items.size))
 
@@ -34,7 +32,7 @@ object DayEleven extends DayChallenge[BigInt, Long] with Helpers {
     splitIntoGroupsOfList(l).map { monkeyLines =>
       val ints = monkeyLines.map(line => extractInts(line))
       val firstIFrom: Int => Int = lineNo =>  ints(lineNo).head
-      val monkeyIndex = firstIFrom(0)
+      val index = firstIFrom(0)
       val items = ints(1).map(i => i.toLong)
       val operation = monkeyLines(2).trim().split(" ").takeRight(2).toList match {
         case "*" :: "old" :: Nil => x: Long => x * x
@@ -42,13 +40,13 @@ object DayEleven extends DayChallenge[BigInt, Long] with Helpers {
         case "+" :: "old" :: Nil => x: Long => x + x
         case "+" :: i :: Nil     => x: Long => x + i.toLong
       }
-      monkeyIndex -> Monkey(monkeyIndex, items, operation, firstIFrom(3), firstIFrom(4), firstIFrom(5), 0)
+      index -> Monkey(index, items, operation, firstIFrom(3), firstIFrom(4), firstIFrom(5), 0)
     }.toMap
 
-  case class Monkey(i: Int, items: List[Long], op: Long => Long, divBy: Int, ifTrue: Int, ifFalse: Int, inspected: Int)
+  case class Monkey(i: Int, items: List[Long], op: Long => Long, divisibleBy: Int, ifTrue: Int, ifFalse: Int, inspected: Int)
 }
 
-object DayElevenData extends TestData[BigInt, BigInt] {
+object DayElevenData extends TestData[Long, Long] {
   override val testData: List[String] = List(
     "Monkey 0:",
     "  Starting items: 79, 98",
@@ -78,6 +76,6 @@ object DayElevenData extends TestData[BigInt, BigInt] {
     "    If true: throw to monkey 0",
     "    If false: throw to monkey 1",
   )
-  override val expectedPartOne: Option[BigInt] = Some(BigInt(10605))
-  override val expectedPartTwo: Option[BigInt] = Some(2713310158L)
+  override val expectedPartOne: Option[Long] = Some(10605L)
+  override val expectedPartTwo: Option[Long] = Some(2713310158L)
 }
