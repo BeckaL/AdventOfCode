a0f1cb73375bbf46577a9205a9e19b492d1195c9
BeckaL <becka.lelew@digital.cabinet-office.gov.uk>
2024-12-03 13:22:32 +0000

    Add Day 3 part 2 2024

--- src/main/scala/AOC_2024/DayThree.scala
+++ src/main/scala/AOC_2024/DayThree.scala
@@ -2,20 +2,42 @@ package AOC_2024
 
 import shared.{DayChallenge, Helpers, TestData}
 
+import scala.annotation.tailrec
+
 object DayThree extends DayChallenge[Long, Long] with Helpers{
   override def partOne(l: List[String]): Long =
     l.map(getSumOfMultiplyInstructions).sum
 
-  private def getSumOfMultiplyInstructions(s: String): Long =
-    val results = """mul\((\d+),(\d+)\)""".r.findAllIn(s).toList
-    results.map { s =>
-      val ints = extractInts(s)
-      ints.head.toLong * ints(1).toLong
-    }.sum
+  override def partTwo(l: List[String]): Long =
+    l.foldLeft((0.toLong, true)){case ((soFar, enabled), string) =>
+      getSumOfMultiplyInstructions(string, enabled, soFar)
+    }._1
 
-  override def partTwo(l: List[String]): Long = {
-    2
-  }
+  private val multiplyRegex = """mul\((\d+),(\d+)\)""".r
+
+  private def getSumOfMultiplyInstructions(s: String): Long =
+    multiplyRegex.findAllIn(s).toList.map(extractInts(_).product).sum
+
+  @tailrec
+  private def getSumOfMultiplyInstructions(s: String, enabled: Boolean, soFar: Long): (Long, Boolean) =
+    val presentInstructions = getPresentInstructions(s)
+    if (presentInstructions.isEmpty)
+      (soFar, enabled)
+    else
+      val (newS, newEnabled, newSoFar) = presentInstructions.minBy(_._1) match
+        case (i, "do") => (s.drop(i + 4), true, soFar)
+        case (i, "dont") => (s.drop(i + 6), false, soFar)
+        case (i, instructionString) =>
+         val soFarAddition = if (enabled) extractInts(instructionString).product else 0
+         (s.drop(i + instructionString.length), enabled, soFar + soFarAddition)
+      getSumOfMultiplyInstructions(newS, newEnabled, newSoFar)
+
+  private def getPresentInstructions(s: String) =
+    List(
+      (s.indexOf("do()"), "do"),
+      (s.indexOf("don't()"), "dont"),
+      multiplyRegex.findFirstMatchIn(s).map(m => (m.start, m.toString)).getOrElse((-1, ""))
+    ).filter(_._1 >= 0)
 }
 
 object DayThreeData extends TestData[Long, Long] {
