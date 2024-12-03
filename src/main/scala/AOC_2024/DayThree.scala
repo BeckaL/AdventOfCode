f3d8dcfe74fb5925c3409c9ad347ab9103d64334
BeckaL <rebecca.lelew@gmail.com>
2024-12-03 09:17:33 +0000

    Minor set up for part 2

--- src/main/scala/AOC_2024/DayThree.scala
+++ src/main/scala/AOC_2024/DayThree.scala
@@ -2,9 +2,7 @@ package AOC_2024
 
 import shared.{DayChallenge, Helpers, TestData}
 
-import scala.annotation.tailrec
-
-object DayThree extends DayChallenge[Long, Int] with Helpers{
+object DayThree extends DayChallenge[Long, Long] with Helpers{
   override def partOne(l: List[String]): Long =
     l.map(getSumOfMultiplyInstructions).sum
 
@@ -15,15 +13,16 @@ object DayThree extends DayChallenge[Long, Int] with Helpers{
       ints.head.toLong * ints(1).toLong
     }.sum
 
-  override def partTwo(l: List[String]): Int = {
+  override def partTwo(l: List[String]): Long = {
     2
   }
 }
 
-object DayThreeData extends TestData[Long, Int] {
+object DayThreeData extends TestData[Long, Long] {
   override val testData: List[String] = List(
     "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
   )
+  override val testData2: Option[List[String]] = Some(List("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"))
   override val expectedPartOne: Option[Long] = Some(161)
-  override val expectedPartTwo: Option[Int] = Some(0)
+  override val expectedPartTwo: Option[Long] = Some(48)
 }
