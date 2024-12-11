af455081961f9169ae0769a867e6b67646d496ce
BeckaL <rebecca.lelew@gmail.com>
2024-12-11 20:33:53 +0000

    Day 9 part 2 - continuing the theme of disgusting code
    very slow but works :-D

--- src/main/scala/AOC_2024/DayNine.scala
+++ src/main/scala/AOC_2024/DayNine.scala
@@ -1,10 +1,10 @@
 package AOC_2024
 
-import shared.{DayChallenge, TestData}
+import shared.{DayChallenge, TestData, UpdaterHelpers}
 
 import scala.annotation.tailrec
 
-object DayNine extends DayChallenge[Long, Int]{
+object DayNine extends DayChallenge[Long, Long] {
   case class MemoryChunk(size: Int, position: Int)
 
   override def partOne(l: List[String]): Long = {
@@ -22,9 +22,8 @@ object DayNine extends DayChallenge[Long, Int]{
     } else if (chunks.size == 1) {
       soFar + score(chunks.head.position, indexInFile, chunks.head.size)
     } else {
-      val toMove = chunks.last
       val (newSoFar, newChunks, newSpaces, newIndexInFile) =
-        moveChunk(toMove, chunks.dropRight(1), spaces, soFar, indexInFile)
+        moveChunk(chunks.last, chunks.dropRight(1), spaces, soFar, indexInFile)
       defrag(newChunks, newSpaces, newSoFar, newIndexInFile)
     }
 
@@ -63,10 +62,52 @@ object DayNine extends DayChallenge[Long, Int]{
       }
     }
 
+  @tailrec
+  private def defragMaintainingFiles(wholeMemory: List[Option[Int]], intsToWholeMemoryIndices: Map[Int, Int], remainingIndicesToTry: List[Int]): List[Option[Int]] =
+    remainingIndicesToTry match
+      case Nil => wholeMemory
+      case index :: _ =>
+        println(s"now on index $index")
+        val newMemory = placeMemoryChunk(intsToWholeMemoryIndices(index), wholeMemory)
+        defragMaintainingFiles(newMemory, intsToWholeMemoryIndices, remainingIndicesToTry.tail)
+
+  private def calculateFinal(wholeMemory: List[Option[Int]]) =
+    wholeMemory.zipWithIndex.map{ case (maybeChunkInitialPosition, endPosition) =>
+      maybeChunkInitialPosition match
+        case None => 0
+        case Some(chunkInitialPosition) => chunkInitialPosition.toLong * endPosition
+    }.sum
+
+  private def placeMemoryChunk(indexToStartAt: Int, wholeMemory: List[Option[Int]]): List[Option[Int]] =
+    val memoryChunkInitialPosition = wholeMemory(indexToStartAt).get
+    val toMove = wholeMemory.slice(indexToStartAt, wholeMemory.size).takeWhile(maybeInt => maybeInt == Some(memoryChunkInitialPosition))
+    val sizeOfToMove = toMove.size
+    val maybeIndexToMoveTo = wholeMemory.slice(0, indexToStartAt).sliding(sizeOfToMove).zipWithIndex.find(_._1.forall(_.isEmpty)).map(_._2)
+    maybeIndexToMoveTo match
+      case None =>
+        wholeMemory
+      case Some(i) =>
+        (0 until sizeOfToMove).foldLeft(wholeMemory){case (mem, offset) =>
+        val memoryAfterUpdating = UpdaterHelpers.updateAtIndex(i + offset, Some(memoryChunkInitialPosition), mem)
+        UpdaterHelpers.updateAtIndex(indexToStartAt + offset, None, memoryAfterUpdating)
+      }
+
+  @tailrec
+  private def constructEntireList(chunks: List[MemoryChunk], spaces: List[Int], soFar: List[Option[Int]], m: Map[Int, Int]): (List[Option[Int]], Map[Int, Int]) =
+    (chunks, spaces) match {
+      case (firstChunk :: _, Nil) =>
+        (soFar ++ (0 until firstChunk.size).map(_ => Some(firstChunk.position)), m.updated(firstChunk.position, soFar.size))
+      case (firstChunk :: _, firstSpace :: __) =>
+        val newSoFar = soFar ++
+          (0 until firstChunk.size).map(_ => Some(firstChunk.position)) ++
+          (0 until firstSpace).map(_ => None)
+        val newM = m.updated(firstChunk.position, soFar.size)
+        constructEntireList(chunks.tail, spaces.tail, newSoFar, newM)
+    }
+
   private def updateScoreAndPositionWithChunk(c: MemoryChunk, indexInFile: Int, soFar: Long): (Long, Int) =
     val scoreOfFirstChunk = score(c.position, indexInFile, c.size)
     (soFar + scoreOfFirstChunk, indexInFile + c.size)
-
 
   private def parseSpaces(s: String): List[Int] =
     val onlySpaces = s.zipWithIndex.collect { case (ch, i) if i % 2 != 0 => ch }.mkString
@@ -76,15 +117,18 @@ object DayNine extends DayChallenge[Long, Int]{
     val onlyMemoryChunks = s.zipWithIndex.collect { case (ch, i) if i % 2 == 0 => ch }.mkString
     onlyMemoryChunks.zipWithIndex.map((char, index) => MemoryChunk(char.toString.toInt, index)).toList
 
-  override def partTwo(l: List[String]): Int = {
-    2
-  }
+  override def partTwo(l: List[String]): Long =
+    val memoryChunks = parseMemoryChunks(l.head)
+    val spaces = parseSpaces(l.head)
+    val (wholeStartingMemory, indicesToStartingPoints) = constructEntireList(parseMemoryChunks(l.head), spaces, List(), Map())
+    val endMemory = defragMaintainingFiles(wholeStartingMemory, indicesToStartingPoints, (0 to memoryChunks.last.position).reverse.toList)
+    calculateFinal(endMemory)
 }
 
-object DayNineData extends TestData[Long, Int] {
+object DayNineData extends TestData[Long, Long] {
   override val testData: List[String] = List(
     "2333133121414131402"
   )
   override val expectedPartOne: Option[Long] = Some(1928)
-  override val expectedPartTwo: Option[Int] = Some(0)
+  override val expectedPartTwo: Option[Long] = Some(2858)
 }
